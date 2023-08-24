f184bd7c020c0c1395e720a85f8f10fbfa5e387b
hjck4433 <hjck4433@gmail.com>
2023-08-24 21:46:36 +0900

    객체수업 20230824 Remainder 수정

--- src/나머지/Remainder.java
+++ src/나머지/Remainder.java
@@ -8,37 +8,37 @@ import java.util.Scanner;
 
 public class Remainder {
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int b = 42; // 고정된 나누는 값
-//        int[] input = new int[10]; // 입력 받을 정수 배열
-//        int[] remain = new int[41]; // 나머지 값으로 나올수 잇는 정수만큼 크기의 배열
-//        int cnt = 0; // 중복되지 않는 값 카운트
-//
-//        for(int i = 0; i < input.length; i++ ) {
-//            input[i] = sc.nextInt();
-//        }
-//
-//        for (int a : input) {
-//            int num = a % b;
-//            if (remain[num] == 0) {
-//                cnt++;
-//                remain[num] = 1;
-//            }
-//        }
-//        System.out.print(cnt);
+        Scanner sc = new Scanner(System.in);
+        int b = 42; // 고정된 나누는 값
+        int[] input = new int[10]; // 입력 받을 정수 배열
+        int[] remain = new int[41]; // 나머지 값으로 나올수 잇는 정수만큼 크기의 배열
+        int cnt = 0; // 중복되지 않는 값 카운트
+
+        for(int i = 0; i < input.length; i++ ) {
+            input[i] = sc.nextInt();
+        }
+
+        for (int a : input) {
+            int num = a % b;
+            if (remain[num] == 0) {
+                cnt++;
+                remain[num] = 1;
+            }
+        }
+        System.out.print(cnt);
 
         // 강사님 풀이
-        int[] arr = new int[42];
-        int cnt = 0; // 42로 나눈 나머지가 다른 경우에 대한 총 횟수
-        int input; // 입력 받은 값
-        Scanner sc = new Scanner(System.in);
-        System.out.print("10개의 수 입력 : ");
-        for (int i = 0; i < 10; i++) {
-            input = sc.nextInt();
-            arr[input % 42] = 1; // 입력 받은 값을 42로 나눈 나머지에 해당하는 인덱스에 1을 대입
-        }
-        for (int val : arr) cnt += val;
-        System.out.println(cnt);
+//        int[] arr = new int[42];
+//        int cnt = 0; // 42로 나눈 나머지가 다른 경우에 대한 총 횟수
+//        int input; // 입력 받은 값
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("10개의 수 입력 : ");
+//        for (int i = 0; i < 10; i++) {
+//            input = sc.nextInt();
+//            arr[input % 42] = 1; // 입력 받은 값을 42로 나눈 나머지에 해당하는 인덱스에 1을 대입
+//        }
+//        for (int val : arr) cnt += val;
+//        System.out.println(cnt);
 
 
     }
